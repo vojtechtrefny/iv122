b1e05987c1cc6a839a8bf37aba994461ce38316d
Vojtech Trefny <vtrefny@redhat.com>
2016-02-28 21:30:18 +0100

    Graficka knihovna -- prvni commit

---
+++ lib/vector.py
@@ -0,0 +1,47 @@
+class Point(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return '<circle cx="{}" cy="{}" r="2" stroke="black" stroke-width="3" ' \
+               'fill="black" />\n'.format(self.x, self.y)
+
+
+class Line(object):
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __str__(self):
+        return '<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="black" ' \
+               'stroke-width="1"/>\n'.format(self.start.x, self.start.y,
+                                              self.end.x, self.end.y)
+
+
+class SVG(object):
+
+    def __init__(self, folder, name):
+        self.name = name
+        self.folder = folder
+        self.objects = []
+
+    def add_line(self, start_x, start_y, end_x, end_y):
+        line = Line(Point(start_x, start_y),
+                    Point(end_x, end_y))
+
+        self.objects.append(line)
+
+    def save(self):
+        with open('/home/vtrefny/Projekty/skola/IV122/' + self.folder + '/' + self.name + '.svg', 'w+') as f:
+            f.write('<svg xmlns="http://www.w3.org/2000/svg">\n')
+
+            for obj in self.objects:
+                f.write(str(obj))
+
+            f.write('</svg>\n')
+
+    def show(self):
+        pass
