b2eae2811c99d0b78e28077725fcf471fb2f2676
Vojtech Trefny <vtrefny@redhat.com>
2016-03-06 15:24:00 +0100

    Graphical library: bitmap version

---
+++ lib/bitmap.py
@@ -0,0 +1,33 @@
+from PIL import Image, ImageDraw
+
+
+class _Point(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class PNG(object):
+
+    def __init__(self, width=400, height=400):
+        self.width = width
+        self.height = height
+
+        self.image = Image.new("RGBA", (self.width, self.height), (255, 255, 255, 255))
+
+        self.draw = ImageDraw.Draw(self.image)
+
+    def draw_line(self, start, end, color="black", width=1):
+        self.draw.line(xy=(start.x, start.y, end.x, end.y), fill=color, width=width)
+
+    def draw_pixel(self, point, color="black"):
+        self.draw.point(xy=(point.x, point.y), fill=color)
+
+    def draw_circle(self, center, radius, fill_color="black", outline_color="black"):
+        bounds = (center.x - radius, center.y - radius,
+                  center.x + radius, center.y + radius)
+        self.draw.ellipse(bounds, fill=fill_color, outline=outline_color)
+
+    def show(self):
+        self.image.show()
