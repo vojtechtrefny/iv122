39810d3b647810f2965143f9c790983a8bab8d77
Vojtech Trefny <vtrefny@redhat.com>
2016-05-17 20:02:49 +0200

    CV2: add steps graph for exponentiation

--- cv2/exponentiation.py
+++ cv2/exponentiation.py
@@ -1,11 +1,18 @@
 import math
+import random
+import time
+import decimal
 
+from collections import namedtuple
+import matplotlib.pyplot as pyplot
+
+decimal.getcontext().prec = 100
 
 class Fraction(object):
 
     def __init__(self, a, b):
-        self.a = a
-        self.b = b
+        self.a = decimal.Decimal(a)
+        self.b = decimal.Decimal(b)
 
     def __str__(self):
         return "%d / %d = %f" % (self.a, self.b, self.a / self.b)
@@ -16,14 +23,14 @@ def simple_exp(x, n):
 
     res = 1
     # exponentiation to n
-    for i in range(n):
+    for i in range(int(n)):
         res *= x
 
     return res
 
 
-def bisection_exp(x, y, precision=4):
-
+def bisection_exp(x, y, precision=0.00001):
+    x = decimal.Decimal(x)
     exp = simple_exp(x, y.a)
 
     # y.b-th root will be between 1 and x^(y.a)
@@ -36,7 +43,7 @@ def bisection_exp(x, y, precision=4):
         exp_guess = simple_exp(guess, y.b)
 
         # check if guess^(y.b) is close enough to exp
-        if abs(exp_guess - exp) < 1 / (simple_exp(10, precision)):
+        if abs(exp_guess - exp) <= precision:
             return (steps, guess)
 
         # change the bisection interval
@@ -48,19 +55,47 @@ def bisection_exp(x, y, precision=4):
         steps += 1
 
 
-def newton_exp(x, y, precision=4):
-    exp = simple_exp(x, y.a)
+def newton_exp(x, y, precision=decimal.Decimal(0.00001)):
+    x = decimal.Decimal(x)
 
-    # y.b-th root will be around x^(y.a) / y.b
-    x_0 = exp / y.b
+    exp = simple_exp(x, y.a)
+    x_0 = x
 
     steps = 0
     while True:
-        x_1 = x_0 - (x_0 / y.b) + exp / (y.b * simple_exp(x_0, y.b - 1))
+        x_1 = x_0 - (simple_exp(x_0, y.b) - exp) / (y.b * simple_exp(x_0, y.b - 1))
 
-        # check if x_1^(y.b) is close enough to exp
-        if abs(simple_exp(x_1, y.b) - exp) < 1 / (simple_exp(10, precision)):
+        # check if x_1^(y.b) is close enough to exp, or if bounds are too close
+        if abs(simple_exp(x_1, y.b) - exp) < precision:
             return (steps, x_1)
 
         x_0 = x_1
         steps += 1
+
+
+Result = namedtuple("Result", ["base", "exp", "bisection", "newton"])
+
+
+def steps_test():
+    results = []
+
+    for i in range(100):
+        print(i)
+        base = random.randint(1, 100)
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        exp = Fraction(a, b)
+
+        res1 = bisection_exp(base, exp, 0.0000000001)
+        res2 = newton_exp(base, exp, 0.0000000001)
+
+        results.append(Result(base=base, exp=exp, bisection=res1[0], newton=res2[0]))
+
+    exps = [str(r.base) + "^" + str(r.exp.a) + "/" + str(r.exp.b) for r in results]
+    newton = [r.newton for r in results]
+    bisection = [r.bisection for r in results]
+
+    pyplot.xticks(range(len(exps)), exps, rotation="vertical")
+    pyplot.plot(range(len(exps)), bisection, "r.")
+    pyplot.plot(range(len(exps)), newton, "g.")
+    pyplot.show()
